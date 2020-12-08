5cd8f7d18f967d247f571cb8827fc87d1b84c0c8
shilpashabin <websoullabsshilpa@gmail.com>
2020-12-08 11:01:59 +0530

    my udaan app

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
 
 import 'package:flutter/material.dart';
-//  import 'package:udaan/Alert.dart';
-//  import 'package:udaan/Register.dart';
-//  import 'package:udaan/Support.dart';
- import 'package:udaan/Return.dart';
-//  import 'package:udaan/Order.dart';
-// import 'package:udaan/Tabbar.dart';
-//  import 'package:udaan/Udaanpay.dart';
-  // import 'package:udaan/Modal.dart';
+  //  import 'package:udaan/Alert/Alert.dart';
+   import 'package:udaan/Alert/Register.dart';
+//   import 'package:udaan/Alert/Support.dart';
+//  import 'package:udaan/Alert/Return.dart';
+  //  import 'package:udaan/Alert/Order.dart';
+  // import 'package:udaan/Alert/Tabbar.dart';
+  import 'package:udaan/Alert/Udaanpay.dart';
+  
 
 void main() => runApp(MaterialApp(
 
-   initialRoute: '/Return',
+   initialRoute: '/Udaanpay',
   routes: {
 
 //  '/Alert': (context)=>Alert(),
-  // '/Register':(context)=>Register(),
+    '/Register':(context)=>Register(),
 //  '/Support':(context)=>Support(),
   // '/Return':(context)=>Return(),
 //  '/Order':(context)=>Order(),
 //  '/Tabbar':(context)=>Tabbar(),
-//  '/Udaanpay':(context)=>Udaanpay(),
- '/Return':(context)=>Return(),
+ '/Udaanpay':(context)=>Udaanpay(),
+//  '/Return':(context)=>Return(),
   },
 ),
 );
